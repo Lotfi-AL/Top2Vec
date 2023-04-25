454b60f588d40f389525e53b57ecb1470f307733
Lotfi <lotfi.amin@hotmail.com>
2023-04-25 16:33:37 +0200

    added distiluse-base-multilingual-cased-v2

--- top2vec/Top2Vec.py
+++ top2vec/Top2Vec.py
@@ -78,7 +78,8 @@ sbert_models = ["distiluse-base-multilingual-cased",
                 "NbAiLab/nb-sbert-base",
                 "all-roberta-large-v1",
                 "all-miniLM-L12-v2",
-                "universal-sentence-encoder-multilingual-v2"]
+                "universal-sentence-encoder-multilingual-v2",
+                "distiluse-base-multilingual-cased-v2"]
 
 
 acceptable_embedding_models = use_models + sbert_models
